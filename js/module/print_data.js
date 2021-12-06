7b466960142543307dd344705d9948a7db4bb299
JeonChanMin <cksals3753@naver.com>
2021-12-06 15:16:51 +0900

    print_data.js

--- js/module/print_data.js
+++ js/module/print_data.js
@@ -1,7 +1,7 @@
 /*
  * @filename : print_data.js
  * @description : 거주 인구 통계를 화면에 출력하는 함수
- * @author : 이슬기 (abcabcp@naver.com)
+ * @author : 이슬기 (abcabcp@naver.com) 전찬민 (cksals3753@gmail.com)
  */
 
 
@@ -29,48 +29,52 @@ function print_data() {
     /**인구수/10000단위*/
 
     //남자인구
+    const th2 = document.createElement("td");
+    th2.innerHTML = Math.floor(v.male);
     //여성인구
+    const th3 = document.createElement("td");
+    th3.innerHTML = Math.floor(v.fmale);
     //10대 인구
-    const th4 = document.createElement("th");
+    const th4 = document.createElement("td");
     th4.innerHTML = Math.floor(v.teenage_cnt);
 
     //20대 인구
-    const th5 = document.createElement("th");
+    const th5 = document.createElement("td");
     th5.innerHTML = Math.floor(v.twenty_cnt);
 
     //30대 인구
-    const th6 = document.createElement("th");
+    const th6 = document.createElement("td");
     th6.innerHTML = Math.floor(v.thirty_cnt);
 
     //40대 인구
-    const th7 = document.createElement("th");
+    const th7 = document.createElement("td");
     th7.innerHTML = Math.floor(v.forty_cnt);
 
     //50대 인구
-    const th8 = document.createElement("th");
+    const th8 = document.createElement("td");
     th8.innerHTML = Math.floor(v.fifty_cnt);
 
     //60대 인구
-    const th9 = document.createElement("th");
+    const th9 = document.createElement("td");
     th9.innerHTML = Math.floor(v.sixty_cnt);
 
     //70대 이상
-    const th10 = document.createElement("th");
+    const th10 = document.createElement("td");
     th10.innerHTML = Math.floor(v.seventy_more_than_cnt);
 
 
-    th1.appendChild(tr);
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(th3);
+    tr.appendChild(th4);
+    tr.appendChild(th5);
+    tr.appendChild(th6);
+    tr.appendChild(th7);
+    tr.appendChild(th8);
+    tr.appendChild(th9);
+    tr.appendChild(th10);
 
-
-    th4.appendChild(tr);
-    th5.appendChild(tr);
-    th6.appendChild(tr);
-    th7.appendChild(tr);
-    th8.appendChild(tr);
-    th9.appendChild(tr);
-    th10.appendChild(tr);
-
-    tr.appendChild(tbody);
+    list.appendChild(tr);
 }
 
 
