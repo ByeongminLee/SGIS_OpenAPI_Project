b84da23734fb48aed917963a4746fbe6a98257cf
ByeongminLee <bmlee531@gmail.com>
2021-12-06 12:36:38 +0900

    create adm_code

---
+++ js/module/adm_code.js
@@ -0,0 +1,106 @@
+/*
+ * @filename : adm_code.js
+ * @description : 행정구역 코드
+ * @author : 이병민 (bmlee531@gmail.com)
+ */
+
+/**
+ *
+ * @param {string} serviceID        서sdf
+ * @param {string} securityKey      sdf
+ * @return {string}      sdfsdf
+ */
+
+const ADM_CODE = {
+    main_code: {
+        Seoul: "11",
+        Busan: "21",
+        Daegu: "22",
+        Incheon: "23",
+        Gwangju: "24",
+        Daejeon: "25",
+        Ulsan: "26",
+        Sejong: "29",
+        Gyeonggi: "31",
+        Gangwon: "32",
+        Chungcheongbuk: "33",
+        Chungcheongnam: "34",
+        Jeonbuk: "35",
+        Jeonnam: "36",
+        Gyeongbuk: "37",
+        Gyeongnam: "38",
+        Jeju: "39",
+    },
+    11: {
+        jongno: "010",
+        Jung: "020",
+        Yongsan: "030",
+        jusoen: "040",
+        Gwangjin: "050",
+        Dongdaemun: "060",
+        Jungnang: "070",
+        Seongdong: "080",
+        Gangbuk: "090",
+        Dobong: "100",
+        Nowon: "110",
+        Eunpyeong: "120",
+        Seodaemun: "130",
+        Mapo: "140",
+        Yangcheon: "150",
+        Gangseo: "160",
+        Guro: "170",
+        Geumcheon: "180",
+        Yeongdeungpo: "190",
+        Dongjak: "200",
+        Gwanak: "210",
+        Seocho: "220",
+        Gangnam: "230",
+        Songpa: "240",
+        Gangdong: "250",
+    },
+    21: {
+        구: "010",
+    },
+    22: {
+        구: "010",
+    },
+    23: {
+        구: "010",
+    },
+    24: {
+        구: "010",
+    },
+    26: {
+        구: "010",
+    },
+    29: {
+        구: "010",
+    },
+    31: {
+        구: "010",
+    },
+    32: {
+        구: "010",
+    },
+    33: {
+        구: "010",
+    },
+    34: {
+        구: "010",
+    },
+    35: {
+        구: "010",
+    },
+    36: {
+        구: "010",
+    },
+    37: {
+        구: "010",
+    },
+    38: {
+        구: "010",
+    },
+    39: {
+        구: "010",
+    },
+};
